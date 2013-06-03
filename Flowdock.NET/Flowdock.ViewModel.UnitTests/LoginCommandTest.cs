6b95fd87c3d244d84fdc833bced1d5c45c4a100c
Matt Greer <mgreer@rallydev.com>
2013-06-03 16:45:00 -0600

    Initial tests for LoginCommand

---
+++ Flowdock.NET/Flowdock.ViewModel.UnitTests/LoginCommandTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using Flowdock.ViewModel.UnitTests.Mocks;
+
+namespace Flowdock.ViewModel.UnitTests {
+	public class LoginCommandTest {
+		private LoginViewModel _loginViewModel;
+		private LoginCommand _command;
+
+		[SetUp]
+		public void BeforeEach() {
+			_loginViewModel = new LoginViewModel(new MockFlowdockContext());
+			_command = new LoginCommand(_loginViewModel);
+		}
+
+		[Test]
+		public void CanExecute_false_if_username_and_password_are_null() {
+			_loginViewModel.Username = null;
+			_loginViewModel.Password = null;
+
+			Assert.That(_command.CanExecute(null), Is.False);
+		}
+
+		[Test]
+		public void CanExecute_false_if_username_and_password_are_blank() {
+			_loginViewModel.Username = "";
+			_loginViewModel.Password = "";
+
+			Assert.That(_command.CanExecute(null), Is.False);
+		}
+
+		[Test]
+		public void CanExecute_false_if_username_and_password_are_whitespace() {
+			_loginViewModel.Username = "   ";
+			_loginViewModel.Password = "      ";
+
+			Assert.That(_command.CanExecute(null), Is.False);
+		}
+
+		[Test]
+		public void CanExecute_true_if_username_and_password_are_set() {
+			_loginViewModel.Username = "hello@kitty.com";
+			_loginViewModel.Password = "somepassword";
+
+			Assert.That(_command.CanExecute(null), Is.True);
+		}
+	}
+}
---
+++ Flowdock.NET/Flowdock.ViewModel.UnitTests/Mocks/MockFlowdockContext.cs
@@ -0,0 +1,14 @@
+﻿using Flowdock.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flowdock.ViewModel.UnitTests.Mocks {
+	public class MockFlowdockContext : IFlowdockContext {
+		public Task<string> Login(string username, string password) {
+			throw new NotImplementedException();
+		}
+	}
+}
